8fd91793f0fa54b417512abb463fa78f1fe5b5d0
jeffb4real <urieow@gmail.com>
2018-11-05 12:42:23 -0800

    Scrape URLs on first three pages and write them to a file

--- scraper.py
+++ scraper.py
@@ -4,26 +4,23 @@ from selenium import webdriver
 # Crawling Pages with Selenium (Part 1/2)
 # https://www.youtube.com/watch?v=zjo9yFHoUl8
 
-
-# Instantiate Chrome Browser and open URL
-driver = webdriver.Chrome()
+# We will augment this URL to navigate to successive pages
 baseURL = "https://sanle.ent.sirsi.net/client/en_US/default/search/results?te=ASSET&isd=true"
-driver.get(baseURL)
 
-# Scrape all URLS on this page
-links = driver.find_elements_by_partial_link_text('sanle')
+driver = webdriver.Chrome()
+#for page in range (0, 2521, 12):
+for page in range (0, 25, 12):
+    # Instantiate Chrome Browser and open URL
+    driver.get(baseURL + '&rw=' + str(page))
 
-# Print out all the scraped links
-for i in range(len(links)):
-    print (links[i].text)
+    # Scrape all URLS on this page and all successive pages
+    links = driver.find_elements_by_partial_link_text('sanle')
+
+    # Save the scraped links in a file
+    with open('links.csv', 'a') as f:
+        for i in range(len(links)):
+            print (links[i].text)
+            f.write ((links[i].text) + "\n")
 
 driver.close()
-
-# How to navigate through all photo archive pages:
-# https://sanle.ent.sirsi.net/client/en_US/default/search/results?      te=ASSET&isd=true
-# https://sanle.ent.sirsi.net/client/en_US/default/search/results?rw=12&te=ASSET&isd=true
-# https://sanle.ent.sirsi.net/client/en_US/default/search/results?rw=24&te=ASSET&isd=true
-# https://sanle.ent.sirsi.net/client/en_US/default/search/results?rw=36&te=ASSET&isd=true
-# ...
-# https://sanle.ent.sirsi.net/client/en_US/default/search/results?rw=2520&te=ASSET&isd=true
 
