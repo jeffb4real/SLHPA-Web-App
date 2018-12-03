951986612f74775c1a56596a37ca4e72f9b31bb4
jeffb4real <urieow@gmail.com>
2018-12-03 15:33:55 -0800

    Add'l functionality: 1) merge Title field from other Excel document as secondary description field, 2) remove unuseful descriptions (e.g. 'Vol. 63'), 3) print a summary report

--- comber.py
+++ comber.py
@@ -19,8 +19,17 @@ subject_group = 10
 geo_coord_original = 11
 geo_coord_UTM = 12
 
+# First, create an array of entries in the Title field, from document on photo archive DVD
+csv_input_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'V01-V64 Index.csv')
+title_fields = []
+with open(csv_input_file, 'r', newline='') as infile:
+    reader = csv.reader(infile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for record in reader:
+        # 2nd array element is the Title field; this is the only useful descriptive field in this document
+        title_fields.append(record[2])
+
 # Derive an output .csv file from our existing .csv file
-csv_input_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'SLHPA-records-phase01.csv')
+csv_input_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'SLHPA-records-phase01-ver1.csv')
 csv_output_file = csv_input_file.replace('.csv', '_' + time.strftime("%Y%m%d-%H%M%S") + '.csv')
 with open(csv_output_file, 'w', newline='') as outfile, open(csv_input_file, 'r', newline='') as infile:
     writer = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -29,10 +38,17 @@ with open(csv_output_file, 'w', newline='') as outfile, open(csv_input_file, 'r'
     # Search title, subject, and description fields for years between 1839 (the
     # invention of photography) and 1980 (approx. culmination of the photo archive).
     # When multiple valid years are found, use the highest one in the date field.
+    num_years_found = 0
+    num_descs_found = 0
+    desc_index = 0
+    num_records = 0
+    num_removed_descs = 0
     first_record = True
     for record in reader:
         date = ''
+        description2 = ''
         list_of_years = []
+        # 1.
         # Match year(s). Notice this will match ca.1872 but won't match ca1872
         # Also, will return 1944 if given 1944-45
         pattern = r'\b(\d\d\d\d)\b'
@@ -40,6 +56,7 @@ with open(csv_output_file, 'w', newline='') as outfile, open(csv_input_file, 'r'
         list_of_years.extend(re.findall(pattern, record[title]))
         list_of_years.extend(re.findall(pattern, record[subject]))
         list_of_years.extend(re.findall(pattern, record[description]))
+        list_of_years.extend(re.findall(pattern, title_fields[desc_index]))
         if (list_of_years):
             print (list_of_years)
             filtered_list_of_years = []
@@ -49,12 +66,30 @@ with open(csv_output_file, 'w', newline='') as outfile, open(csv_input_file, 'r'
             if (filtered_list_of_years):
                 date = str(max(filtered_list_of_years))
                 print ("--------> %s" % date)
+                num_years_found+=1
+
+        # 2.
+        # Compare description fields; add description2 if they don't match
+        if ((title_fields[desc_index] not in record[description]) and
+            (title_fields[desc_index] not in record[title]) and
+            (title_fields[desc_index] != 'NR') ):
+            description2 = title_fields[desc_index]
+            num_descs_found+=1
         print ('----')
+
+        # 3.
+        # Don't need to keep unuseful descriptions
+        description1 = record[description]
+        if (re.match(r'Vol\.\s+\d+$', description1)):
+            description1 = ''
+            num_removed_descs+=1
 
         # Write entire record to output .csv file, populating date field (if a date was found)
         # Special exception for the first record (the header)
         if (first_record):
             date = 'date'
+            description1 = 'description'
+            description2 = 'description_from_DVD'
             first_record = False
         writer.writerow([
             record[resource_name],
@@ -62,7 +97,8 @@ with open(csv_output_file, 'w', newline='') as outfile, open(csv_input_file, 'r'
             record[file_size],
             record[title],
             record[subject],
-            record[description],
+            description1,
+            description2,
             record[contributor],
             record[digital_format],
             record[url_for_file],
@@ -71,4 +107,13 @@ with open(csv_output_file, 'w', newline='') as outfile, open(csv_input_file, 'r'
             record[geo_coord_original],
             record[geo_coord_UTM],
         ])
+
+        desc_index+=1
+        num_records+=1
+
+print ("**Found %d title fields" % len(title_fields))
+print ("**Processed %d records" % num_records)
+print ("\n**Found and added %d years to new .csv file" % num_years_found)
+print ("**Added %d descriptions to new .csv file" % num_descs_found)
+print ("**Removed %d unuseful descriptions\n" % num_removed_descs)
 
