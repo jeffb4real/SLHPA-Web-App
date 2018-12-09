912a30f70afcc3e50b6f7d0390ea4f72f971f7a7
CK <chris.keith@gmail.com>
2018-12-08 16:33:04 -0800

    Add function that takes a coordinate point.

--- transformer.py
+++ transformer.py
@@ -1,10 +1,11 @@
-# transform from map coordinates to GPS
+# Transform from 1980 paper map coordinates to GPS
 
 def map(value, fromStart, fromEnd, toStart, toEnd):
     if (fromEnd == fromStart):
         return value
     return (value - fromStart) * (toEnd - toStart) / (fromEnd - fromStart) + toStart;
 
+# Gotten from manually observing existing data
 topCoord = 18
 bottomCoord = 46
 topGPS = 37.73595
@@ -18,21 +19,32 @@ rightCoord = 'E9'
 leftGPS = -122.19608
 rightGPS = -122.12383
 
+asciiA = ord('A')
+ascii0 = ord('0')
+
 # Turns a string of the form [A-Z][0-9] into a number between 0 and whatever.
 def toNumeric(coord):
-    asciiA = ord('A')
     alphaCoord = ord(coord[0])
-    ascii0 = ord('0')
     numericCoord = ord(coord[1])
     return ((alphaCoord - asciiA) * 10) + (numericCoord - ascii0)
 
 def transformHorizontal(value):
     return map(toNumeric(value), toNumeric(leftCoord), toNumeric(rightCoord), leftGPS, rightGPS)
 
+# Turns a string of the form [A-Z][0-9][0-9][0-9] into GPS coordinates.
+def transformPoint(coords):
+    tensCoord = ord(coords[2])
+    onesCoord = ord(coords[3])
+    verticalCoord = transformVertical(((tensCoord - ascii0) * 10) + (onesCoord - ascii0))
+    horizontalCoord = transformHorizontal(coords[0:2])
+    return [verticalCoord, horizontalCoord]
+
 def test():
     print('transform top: ' + str(transformVertical(topCoord)))
     print('transform bottom: ' + str(transformVertical(bottomCoord)))
     print('transform left: ' + str(transformHorizontal(leftCoord)))
     print('transform right: ' + str(transformHorizontal(rightCoord)))
+    print('transform upper left: ' + str(transformPoint(leftCoord + '18')))
+    print('transform lower right: ' + str(transformPoint(rightCoord + '46')))
 
 test()
