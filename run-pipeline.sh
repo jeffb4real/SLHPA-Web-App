50dd277a777d05667dd1771e0db637557a76dc29
CK <chris.keith@gmail.com>
2018-12-21 07:46:47 -0800

    Fail early if any errors.

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,5 +1,5 @@
 cd ~/Documents/Github/SLHPA-web-app
-python merger.py
-python comber.py
-python transformer.py
-python mapper.py
+python merger.py            ; if [ $? -ne 0 ] ; then exit -6 ; fi
+python comber.py            ; if [ $? -ne 0 ] ; then exit -6 ; fi
+python transformer.py       ; if [ $? -ne 0 ] ; then exit -6 ; fi
+python mapper.py            ; if [ $? -ne 0 ] ; then exit -6 ; fi
