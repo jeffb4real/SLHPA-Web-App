e40de7c9ad01fc20be928a9c08fceee602d5ceb7
jeffb4real <urieow@gmail.com>
2018-12-24 23:26:04 -0800

    Fixed subject field locator, now subject metadata is scraped too.

--- scraper.py
+++ scraper.py
@@ -81,8 +81,9 @@ def scan_pages(driver, more_pages, skipped_pages):
             # This XPath returns an iterable list of records found on this search page
             list_of_records = driver.find_elements(By.XPATH, "//div[contains(@id,'syndeticsImg')]")
 
-            # Special index for Description field, because records can have
-            # more than 1 description element.
+            # Special index variables for subject and description fields (in lieu of record_index),
+            # because records can have more than 1 of these types of elements.
+            subj_index = 0
             desc_index = 0
 
             # All search pages return 12 records except the last page, which has only 6 records.
@@ -93,15 +94,11 @@ def scan_pages(driver, more_pages, skipped_pages):
                 this_record = CSVrecord()
 
                 # Open the white sub-page for this record
-                # log("record_index: " + str(record_index))
-                # log("absolute_record_number: " + str(absolute_record_number))
                 record = list_of_records[record_index]
                 record.click()
                 time.sleep(1)
 
                 # Resource Name
-                # We do an explit wait on this find command, because Resource Name is the first
-                # element on the page.
                 for i in range (0, 10):
                     resource_name = wait.until(EC.presence_of_all_elements_located((By.XPATH,
                         "//div[@class='displayElementText RESOURCE_NAME']")))
@@ -134,25 +131,27 @@ def scan_pages(driver, more_pages, skipped_pages):
                 if (not is_bad_value(title, record_index)):
                     this_record.title = title[record_index].text
 
-                # TODO: fix this
                 # Subject
-                #//div[@class='displayElementLabel SUBJECT_label']//div[@class='displayElementText']//table
-                # subject = record.find_elements(By.XPATH,
-                #     "//div[@class='displayElementText']//table/tbody/tr")
-                # if (subject):
-                #     rows = subject.find_elements(By.TAG_NAME, 'tr')
-
+                subject = record.find_elements(By.XPATH,
+                    "//div[@class='detail_biblio resource_margin']//table//td")
+                if (not is_bad_value(subject, subj_index)):
+                    this_record.subject = '| '
+                    for i in range(subj_index, len(subject), 1):
+                        this_record.subject += subject[i].text + ' | '
+                    this_record.subject = this_record.subject.rstrip()
+                    subj_index = len(subject)
+                
                 # Description
                 description = record.find_elements(By.XPATH,
                     "//div[@class='displayElementText DESCRIPTION']")
                 if (not is_bad_value(description, desc_index)):
-                    # Special handling because a record can contain multiple description elements.
                     for i in range(desc_index, len(description), 1):
                         if (re.findall(r'\.[\"\'\s]*$', description[i].text)):
                             this_record.description += description[i].text + ' '
                         else:
                             this_record.description += description[i].text + '. '
-                    desc_index = len(description)                
+                    this_record.description = this_record.description.rstrip()
+                    desc_index = len(description)
 
                 # Contributor
                 contributor = record.find_elements(By.XPATH,
