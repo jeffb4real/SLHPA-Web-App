0969b54831afc9b264d0de0c9b468f1833612eb4
CK <chris.keith@gmail.com>
2018-12-28 09:37:05 -0800

    Remove unused script.

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,8 +1,4 @@
 cd ~/Documents/Github/SLHPA-web-app
 python merger.py            ; if [ $? -ne 0 ] ; then exit -6 ; fi
-
-# Currently deprecated.
-# python comber.py            ; if [ $? -ne 0 ] ; then exit -6 ; fi
-
 python transformer.py       ; if [ $? -ne 0 ] ; then exit -6 ; fi
 python mapper.py            ; if [ $? -ne 0 ] ; then exit -6 ; fi
