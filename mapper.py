38354edb29353743373b10e3a0c3c3d6e1167f12
CK <chris.keith@gmail.com>
2018-12-29 08:25:09 -0800

    Add photo link to description. Write third KML file.

--- mapper.py
+++ mapper.py
@@ -3,11 +3,9 @@ import csv
 import datetime
 import sys
 
-
 def log(message):
     script_name = sys.argv[0]
     print(str(datetime.datetime.now()) + '\t'+ script_name + ': ' + message)
-
 
 def handle_record(lines, record, column_name):
     geo_coords = record[column_name]
@@ -15,12 +13,10 @@ def handle_record(lines, record, column_name):
         coords = geo_coords.replace('[', '').replace(']', '').split(',')
         if len(coords) == 2:
             lines.append('  <Placemark>\n')
-            lines.append('    <name>' + record['asset_name'].replace('&', 'and') + '</name>\n')
-            description = record['title'].replace('&','and') + ' / ' + record['resource_name'] + ' / ' + record['geo_coord_original']
-            if record.get('address'):
-                description += ' / ' + record['address']
-            if record.get('paper_page_number'):
-                description += ' / ' + record['paper_page_number']
+            lines.append('    <name>' + record['title'].replace('&', 'and') + '</name>\n')
+            description = record['description'].replace('&','and')
+            description += ' [' + record['url_for_file'] + ']' 
+            description += ' [' + record['resource_name'].replace('.pdf', '') + ']'
             lines.append('    <description>' + description + '</description>\n')
             lines.append('    <Point>\n')
             lines.append('    <coordinates>' + coords[0].strip() + ',' + coords[1].strip() + ',0' + '</coordinates>\n')
@@ -32,15 +28,13 @@ def handle_record(lines, record, column_name):
     else:
         return 0
 
-
 def write_kml_file(added_records, lines, kml_file_index, filename_prefix):
     lines.append('</Document>\n')
     lines.append('</kml>\n')
     fn = 'data/' + filename_prefix + '_SLHPA_' + str(kml_file_index) + '.kml'
     with open(fn, 'w+') as kml_file:
         kml_file.writelines(lines)
-    log("{: >4d}".format(added_records) + ' written. ' + fn)
-
+    log("{: >4d}".format(added_records) + ' records written to ' + fn)
 
 def transform_to_kml(input_stream, filename_prefix, column_name):
     reader = csv.DictReader(input_stream, delimiter=',',
@@ -51,7 +45,9 @@ def transform_to_kml(input_stream, filename_prefix, column_name):
     added_records = 0
     lines = ['<?xml version="1.0" encoding="UTF-8"?>\n', '<kml xmlns="http://www.opengis.net/kml/2.2">\n', '<Document>\n']
     for record in reader:
-        added_records += handle_record(lines, record, column_name)
+        # For the calculated reference GPS coordinate layer, only add GPS coords if there are verified geographic coords.
+        if 'calced_ref' != filename_prefix or record.get('verified_gps_coords'):
+            added_records += handle_record(lines, record, column_name)
         total_records_processed += 1
         if added_records == MAX_RECORDS_PER_KML:
             write_kml_file(added_records, lines, kml_file_index, filename_prefix)
@@ -60,13 +56,12 @@ def transform_to_kml(input_stream, filename_prefix, column_name):
             lines = ['<?xml version="1.0" encoding="UTF-8"?>\n', '<kml xmlns="http://www.opengis.net/kml/2.2">\n', '<Document>\n']
     if added_records > 0:
         write_kml_file(added_records, lines, kml_file_index, filename_prefix)
-    log("{: >4d}".format(total_records_processed) + ' records processed')
-
+    log("{: >4d}".format(total_records_processed) + ' input records processed')
 
 def main():
     transform_to_kml(open('data/transformed.csv'), 'calced', 'geo_coord_UTM')
+    transform_to_kml(open('data/transformed.csv'), 'calced_ref', 'geo_coord_UTM')
     transform_to_kml(open('data/transformed.csv'), 'manual', 'verified_gps_coords')
-
 
 if '__main__' == __name__:
     main()
