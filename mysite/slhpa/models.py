d5cdc299fadb686443a2941570532f1ade485cce
CK <chris.keith@gmail.com>
2019-02-01 08:09:45 -0800

    Add missing files.

---
+++ mysite/slhpa/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# 0-column names from transformed.csv not included:
+# asset_name
+# description2
+# digital_format
+# file_size
+# Index Number
+# Notes
+# paper_page_number
+# resource_number
+# source_index
+# Title
+# Volume
+
+class PhotoRecord(models.Model):
+    address = models.CharField(max_length=100)
+    contributor = models.CharField(max_length=1000)
+    description = models.CharField(max_length=2000)
+    geo_coord_original = models.CharField(max_length=100)
+    geo_coord_UTM = models.CharField(max_length=100)
+    period_date = models.CharField(max_length=100)
+    resource_name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=1000)
+    title = models.CharField(max_length=200)
+    url_for_file = models.CharField(max_length=500)
+    verified_gps_coords = models.CharField(max_length=100)
+    year = models.IntegerField()
