f5788459eb07ac20410f263ae4d0072b3bc0425f
CK <chris.keith@gmail.com>
2019-02-12 12:42:25 -0800

    Edit page: make edit fields more useful. Put pix on top and fix size to pixels.

--- mysite/slhpa/forms.py
+++ mysite/slhpa/forms.py
@@ -4,12 +4,19 @@ from .models import PhotoRecord
 
 class EditPhotoMetadataForm(forms.ModelForm):
 
+    title = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols': 60}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'cols': 60}))
     year = forms.IntegerField(required=False)
-    verified_gps_coords = forms.CharField(required=False)
-    address = forms.CharField(required=False)
-    contributor = forms.CharField(required=False)
-    period_date = forms.CharField(required=False)
-    subject = forms.CharField(required=False)
+    verified_gps_coords = forms.CharField(required=False, 
+            widget=forms.Textarea(attrs={'rows': 1, 'cols': 60}))
+    address = forms.CharField(required=False, 
+            widget=forms.Textarea(attrs={'rows': 1, 'cols': 60}))
+    contributor = forms.CharField(required=False, 
+            widget=forms.Textarea(attrs={'rows': 1, 'cols': 60}))
+    period_date = forms.CharField(required=False, 
+            widget=forms.Textarea(attrs={'rows': 1, 'cols': 60}))
+    subject = forms.CharField(required=False, 
+            widget=forms.Textarea(attrs={'rows': 1, 'cols': 60}))
 
     class Meta:
         model = PhotoRecord
