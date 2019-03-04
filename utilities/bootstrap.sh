bb8f65fd4745d741e887a0ba5cdc853e7690e72f
CK <chris.keith@gmail.com>
2019-03-04 12:31:11 -0800

    Make comments into echos.

--- utilities/bootstrap.sh
+++ utilities/bootstrap.sh
@@ -9,8 +9,8 @@ venv/Scripts/activate                                   ; if [ $? -ne 0 ] ; then
 python -m pip install selenium                          ; if [ $? -ne 0 ] ; then exit -6 ; fi
 python -m pip install lxml                              ; if [ $? -ne 0 ] ; then exit -6 ; fi
 
-# To verify that web scraping works:
-# python scraper.py
+echo "To verify that web scraping works, run:"
+echo "python scraper.py"
 
 ./run-pipeline.sh                                       ; if [ $? -ne 0 ] ; then exit -6 ; fi
 
@@ -20,8 +20,8 @@ cd mysite                                               ; if [ $? -ne 0 ] ; then
 python manage.py migrate                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
 python manage.py runserver                              ; if [ $? -ne 0 ] ; then exit -6 ; fi
 
-# Copy image files. They must be correctly distributed into numbered subdirectories.
-# cp -r ~/Downloads/'San Leandro Historical Photo Archive - 311MB/photos - 640x only'/* slhpa/static/slhpa/images/photos/
+echo "Example command line to copy image files. They must be correctly distributed into numbered subdirectories."
+echo "cp -r ~/Downloads/'San Leandro Historical Photo Archive - 311MB/photos - 640x only'/* slhpa/static/slhpa/images/photos/"
 
-# Use browser to import csv into sqlite:
-# http://127.0.0.1:8000/slhpa/import/transformed
+echo "Use browser to import csv into sqlite with this URL:"
+echo "http://127.0.0.1:8000/slhpa/import/transformed"
