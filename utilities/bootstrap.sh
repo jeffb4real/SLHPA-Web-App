c2e3faa4569316c28b95bdc23f8eae7073f645eb
CK <chris.keith@gmail.com>
2019-03-04 13:32:48 -0800

    Run Django last so all echos happen.

--- utilities/bootstrap.sh
+++ utilities/bootstrap.sh
@@ -28,10 +28,11 @@ python -m pip install django                            ; if [ $? -ne 0 ] ; then
 python -m pip install django-tables2                    ; if [ $? -ne 0 ] ; then exit -6 ; fi
 cd mysite                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
 python manage.py migrate                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
-python manage.py runserver                              ; if [ $? -ne 0 ] ; then exit -6 ; fi
 
 echo "Example command line to copy image files. They must be correctly distributed into numbered subdirectories."
 echo "cp -r ~/Downloads/'San Leandro Historical Photo Archive - 311MB/photos - 640x only'/* slhpa/static/slhpa/images/photos/"
 
 echo "Use browser to import csv into sqlite with this URL:"
 echo "http://127.0.0.1:8000/slhpa/import/transformed"
+
+python manage.py runserver
