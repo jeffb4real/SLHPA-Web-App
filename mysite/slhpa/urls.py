a666c540ff83326db475495554612ea64740043e
CK <chris.keith@gmail.com>
2019-03-26 09:50:01 -0700

    Only allow compare page in debug mode.

--- mysite/slhpa/urls.py
+++ mysite/slhpa/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf import settings
 
 from . import views
 
@@ -8,10 +9,12 @@ urlpatterns = [
     path('detail/<str:pk>/', views.DetailView.as_view(), name='detail'),
     path('edit/<str:id>/', views.bound_form, name='edit'),
     path('add/', views.add, name='add'),
-    path('compare/<str:resource_name>/', views.photo_compare, name='compare'),
 
     # 'import' is a reserved word, can't do views.import .
-    path('import/<import_filename>', views.loaddb, name='loaddb'),
-    path('export/<export_filename>', views.export, name='export'),
+    path('import/<str:import_filename>', views.loaddb, name='loaddb'),
+    path('export/<str:export_filename>', views.export, name='export'),
     path('data/<str:filename>', views.datafile, name='data_file'),
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(path('compare/<str:resource_name>/', views.photo_compare, name='compare'))
