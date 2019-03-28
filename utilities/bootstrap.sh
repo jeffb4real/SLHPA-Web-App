93f6947c6c6d3f80182193c338370062a7e26c2d
CK <chris.keith@gmail.com>
2019-03-28 13:58:02 -0700

    Fix paths in photo cp.

--- utilities/bootstrap.sh
+++ utilities/bootstrap.sh
@@ -25,10 +25,14 @@ echo "python scraper.py"
 
 cd mysite                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
 python manage.py migrate                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
-python manage.py createsuperuser                        ; if [ $? -ne 0 ] ; then exit -6 ; fi
+if [ "$WINDIR" = "" ] ; then
+    python manage.py createsuperuser                    ; if [ $? -ne 0 ] ; then exit -6 ; fi
+else
+    echo "run 'python manage.py createsuperuser' in a CMD"  ; if [ $? -ne 0 ] ; then exit -6 ; fi
+fi
 
 echo "Example command line to copy image files. They must be correctly distributed into numbered subdirectories."
-echo "cp -r ~/Downloads/'San Leandro Historical Photo Archive - 311MB/photos - 640x only'/* slhpa/static/slhpa/images/photos/"
+echo "time cp -r ~/Documents/'San Leandro Historical Photo Archive - 311MB/photos - 640x only'/*  ~/Documents/Github/SLHPA-Web-App/mysite/slhpa/static/slhpa/images/photos/"
 
 echo "Use browser to import csv into sqlite with this URL:"
 echo "http://127.0.0.1:8000/slhpa/import/transformed"
