63d5c33c905fa28ad17d40726d6f397df01fdd5d
CK <chris.keith@gmail.com>
2019-03-28 15:42:34 -0700

    Use a small subset of photos for testing script.

--- utilities/deploy.sh
+++ utilities/deploy.sh
@@ -7,16 +7,20 @@ if [ "$TMP" = "" ] ; then
 fi
 
 cd ~/Documents/Github/SLHPA-Web-App/mysite                                  ; if [ $? -ne 0 ] ; then exit -6 ; fi
-sed -i'.bak' 's/ALLOW_EDIT = True/ALLOW_EDIT = False/' mysite/settings.py   ; if [ $? -ne 0 ] ; then exit -6 ; fi
+sed -i 's/ALLOW_EDIT = True/ALLOW_EDIT = False/' mysite/settings.py         ; if [ $? -ne 0 ] ; then exit -6 ; fi
 
 # Do this once the base functionality is working.
-# sed -i'.bak' 's/DEBUG = True/DEBUG = False/' mysite/settings.py             ; if [ $? -ne 0 ] ; then exit -6 ; fi
+# sed -i 's/DEBUG = True/DEBUG = False/' mysite/settings.py                 ; if [ $? -ne 0 ] ; then exit -6 ; fi
 # TODO : ...and run Django with insecure flag
 
-for t in hr lr mr ; do
-    mv slhpa/static/slhpa/images/photos/${t}/ $TMP/                         # ignore failures
+rm -rf $TMP/photos/                                                         ; if [ $? -ne 0 ] ; then exit -6 ; fi
+mv -f slhpa/static/slhpa/images/photos/ $TMP/                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
+mkdir -p slhpa/static/slhpa/images/photos/1/                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
+for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 ; do
+    cp $TMP/photos/1/000000${i}.jpg slhpa/static/slhpa/images/photos/1/     ; if [ $? -ne 0 ] ; then exit -6 ; fi
 done
 
+rm -rf mysite/static/                                                       ; if [ $? -ne 0 ] ; then exit -6 ; fi
 python manage.py collectstatic                                              ; if [ $? -ne 0 ] ; then exit -6 ; fi
 echo "Now run the following command in a CMD shell:"
 echo "gcloud app deploy"
@@ -24,10 +28,9 @@ read -p "Press any key to continue after deploy finishes... " -n1 -s
 
 # TODO : how to run 'python manage.py migrate' on GCP server?
 
-# Now restore repository state
-for t in hr lr mr; do
-    mv $TMP/${t} slhpa/static/slhpa/images/photos/                          # ignore failures
-done
-sed -i'.bak' 's/DEBUG = False/DEBUG = True/' mysite/settings.py             ; if [ $? -ne 0 ] ; then exit -6 ; fi
-sed -i'.bak' 's/ALLOW_EDIT = False/ALLOW_EDIT = True/' mysite/settings.py   ; if [ $? -ne 0 ] ; then exit -6 ; fi
-rm mysite/settings.py.bak                                                   ; if [ $? -ne 0 ] ; then exit -6 ; fi
+# Now restore repository state.
+# Ignore failures to get as much cleaned up as possible.
+rm -rf slhpa/static/slhpa/images/photos/
+mv -f $TMP/photos/ slhpa/static/slhpa/images/
+sed -i 's/DEBUG = False/DEBUG = True/' mysite/settings.py
+sed -i 's/ALLOW_EDIT = False/ALLOW_EDIT = True/' mysite/settings.py
