c2fd6f482ee7362b1076c24f2bf9c271315ca725
CK <chris.keith@gmail.com>
2019-03-28 13:07:54 -0700

    One small step towards deployment.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -137,4 +137,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+# ??? STATIC_ROOT = 'mysite/slhpa/static'
 ALLOW_EDIT = True
--- utilities/deploy.sh
+++ utilities/deploy.sh
@@ -8,15 +8,23 @@ fi
 
 cd ~/Documents/Github/SLHPA-Web-App/mysite                                  ; if [ $? -ne 0 ] ; then exit -6 ; fi
 sed -i'.bak' 's/ALLOW_EDIT = True/ALLOW_EDIT = False/' mysite/settings.py   ; if [ $? -ne 0 ] ; then exit -6 ; fi
+
+# Do this once the base functionality is working.
+# sed -i'.bak' 's/DEBUG = True/DEBUG = False/' mysite/settings.py             ; if [ $? -ne 0 ] ; then exit -6 ; fi
+# TODO : ...and run Django with insecure flag
+
 for t in hr lr mr ; do
     mv slhpa/static/slhpa/images/photos/${t}/ $TMP/                         ; if [ $? -ne 0 ] ; then exit -6 ; fi
 done
 
-# TODO : add comands to deploy here
+python manage.py collectstatic                                              ; if [ $? -ne 0 ] ; then exit -6 ; fi
+# gcloud app deploy                                                         ; if [ $? -ne 0 ] ; then exit -6 ; fi
+
 
 # Now restore repository state
 for t in hr lr mr; do
     mv $TMP/${t} slhpa/static/slhpa/images/photos/                          ; if [ $? -ne 0 ] ; then exit -6 ; fi
 done
+sed -i'.bak' 's/DEBUG = False/DEBUG = True/' mysite/settings.py             ; if [ $? -ne 0 ] ; then exit -6 ; fi
 sed -i'.bak' 's/ALLOW_EDIT = False/ALLOW_EDIT = True/' mysite/settings.py   ; if [ $? -ne 0 ] ; then exit -6 ; fi
 rm mysite/settings.py.bak                                                   ; if [ $? -ne 0 ] ; then exit -6 ; fi
