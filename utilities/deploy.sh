5eb9d9e08ee50d022f46279d6706b840f5267c2c
CK <chris.keith@gmail.com>
2019-03-30 13:53:09 -0700

    Try to deploy all photos.

--- utilities/deploy.sh
+++ utilities/deploy.sh
@@ -7,19 +7,14 @@ if [ "$TMP" = "" ] ; then
 fi
 
 cd ~/Documents/Github/SLHPA-Web-App/mysite                                  ; if [ $? -ne 0 ] ; then exit -6 ; fi
+mv -f slhpa/static/slhpa/images/photos/hr/ $TMP/                            ; if [ $? -ne 0 ] ; then exit -6 ; fi
+mv -f slhpa/static/slhpa/images/photos/lr/ $TMP/                            ; if [ $? -ne 0 ] ; then exit -6 ; fi
+mv -f slhpa/static/slhpa/images/photos/mr/ $TMP/                            ; if [ $? -ne 0 ] ; then exit -6 ; fi
+
 sed -i 's/ALLOW_EDIT = True/ALLOW_EDIT = False/' mysite/settings.py         ; if [ $? -ne 0 ] ; then exit -6 ; fi
 
-# Do this once the base functionality is working.
+# Do this if we can figure how how to run Django with the insecure flag in GCP.
 # sed -i 's/DEBUG = True/DEBUG = False/' mysite/settings.py                 ; if [ $? -ne 0 ] ; then exit -6 ; fi
-# TODO : ...and run Django with insecure flag
-
-rm -rf $TMP/photos/                                                         ; if [ $? -ne 0 ] ; then exit -6 ; fi
-mv -f slhpa/static/slhpa/images/photos/ $TMP/                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
-mkdir -p slhpa/static/slhpa/images/photos/1/                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
-cp $TMP/photos/*.png slhpa/static/slhpa/images/photos/                      ; if [ $? -ne 0 ] ; then exit -6 ; fi
-for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 ; do
-    cp $TMP/photos/1/000000${i}.jpg slhpa/static/slhpa/images/photos/1/     ; if [ $? -ne 0 ] ; then exit -6 ; fi
-done
 
 rm -rf static/                                                              ; if [ $? -ne 0 ] ; then exit -6 ; fi
 python manage.py collectstatic                                              ; if [ $? -ne 0 ] ; then exit -6 ; fi
@@ -36,7 +31,8 @@ fi
 
 # Now restore repository state.
 # Ignore failures to get as much cleaned up as possible.
-rm -rf slhpa/static/slhpa/images/photos/
-mv -f $TMP/photos/ slhpa/static/slhpa/images/
-sed -i 's/DEBUG = False/DEBUG = True/' mysite/settings.py
+mv -f $TMP/hr/ slhpa/static/slhpa/images/photos/
+mv -f $TMP/mr/ slhpa/static/slhpa/images/photos/
+mv -f $TMP/lr/ slhpa/static/slhpa/images/photos/
+# sed -i 's/DEBUG = False/DEBUG = True/' mysite/settings.py
 sed -i 's/ALLOW_EDIT = False/ALLOW_EDIT = True/' mysite/settings.py
