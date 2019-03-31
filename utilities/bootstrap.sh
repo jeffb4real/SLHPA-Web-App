1f874ce9b6c7f87dad2b13385223af16ae479094
CK <chris.keith@gmail.com>
2019-03-31 15:46:41 -0700

    Prevent script from running on Windows.

--- utilities/bootstrap.sh
+++ utilities/bootstrap.sh
@@ -2,40 +2,34 @@
 
 set -x
 
+if [ ! "$WINDIR" = "" ] ; then
+    echo "Do not run on Windows. Use CMD instead."
+    exit -1
+fi
+
 curdir=`pwd`
 base=`basename $curdir`
 if [ ! "$base" = "SLHPA-Web-App" ] ; then
     echo "Must run from within SLHPA-Web-App directory."
     exit -1
 fi
+
 python -m venv venv                                     ; if [ $? -ne 0 ] ; then exit -6 ; fi
-
-if [ "$WINDIR" = "" ] ; then
-    source venv/bin/activate                            ; if [ $? -ne 0 ] ; then exit -6 ; fi
-else
-    echo "Still issues to be worked out in git bash on windows."
-    # venv/Scripts/activate                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
-fi
-
+source venv/bin/activate                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
 pip install -r requirements.txt                         ; if [ $? -ne 0 ] ; then exit -6 ; fi
 
-echo "To verify that web scraping works, run:"
-echo "python scraper.py"
-
 ./run-pipeline.sh                                       ; if [ $? -ne 0 ] ; then exit -6 ; fi
-
 cd mysite                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
 python manage.py migrate                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
-if [ "$WINDIR" = "" ] ; then
-    python manage.py createsuperuser                    ; if [ $? -ne 0 ] ; then exit -6 ; fi
-else
-    echo "run 'python manage.py createsuperuser' in a CMD"  ; if [ $? -ne 0 ] ; then exit -6 ; fi
-fi
+python manage.py createsuperuser                        ; if [ $? -ne 0 ] ; then exit -6 ; fi
 
 echo "Example command line to copy image files. They must be correctly distributed into numbered subdirectories."
 echo "time cp -r ~/Documents/'San Leandro Historical Photo Archive - 311MB/photos - 640x only'/*  ~/Documents/Github/SLHPA-Web-App/mysite/slhpa/static/slhpa/images/photos/"
 
 echo "Use browser to import csv into sqlite with this URL:"
 echo "http://127.0.0.1:8000/slhpa/import/transformed"
+
+echo "To verify that web scraping works, run:"
+echo "python scraper.py"
 
 python manage.py runserver
