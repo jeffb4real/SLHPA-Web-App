793962d30da5cb5934ed18876c21e637c82852d3
CK <chris.keith@gmail.com>
2019-04-05 14:31:43 -0700

    CMD deploy script works.

---
+++ utilities/deploy.cmd
@@ -0,0 +1,22 @@
+rem : CMD script because the gcloud command line utility (sometimes) doesn't echo in git bash.
+rem : python and gloud commands require user input, so this script cannot be run unattended. 
+rem : Takes a few minutes.
+
+pause "Set ALLOW_EDIT = False in mysite/mysite/settings.py."
+
+cd %HOMEDRIVE%%HOMEPATH%\Documents\Github\SLHPA-Web-App\mysite\
+                                                        IF %ERRORLEVEL% NEQ 0 goto :eof
+rem : If these don't exist, just continue.
+move slhpa\static\slhpa\images\photos\hr\ %TMP%\
+move slhpa\static\slhpa\images\photos\mr\ %TMP%\
+move slhpa\static\slhpa\images\photos\lr\ %TMP%\
+
+cmd /c "python manage.py collectstatic"
+                                                        IF %ERRORLEVEL% NEQ 0 goto :eof
+move slhpa\static\slhpa\images\photos %TMP%
+                                                        IF %ERRORLEVEL% NEQ 0 goto :eof
+cmd /c "gcloud app deploy"
+                                                        IF %ERRORLEVEL% NEQ 0 goto :eof
+move %TMP%\photos slhpa\static\slhpa\images
+
+pause "Set back ALLOW_EDIT = True."
