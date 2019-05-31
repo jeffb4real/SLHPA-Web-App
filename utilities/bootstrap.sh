8f5445ce508bb5b02299119b199dd0f38b681130
CK <chris.keith@gmail.com>
2019-05-31 07:41:11 -0700

    Fix for git bash on Windows.

--- utilities/bootstrap.sh
+++ utilities/bootstrap.sh
@@ -2,11 +2,6 @@
 
 set -x
 
-if [ ! "$WINDIR" = "" ] ; then
-    echo "Do not run on Windows. Use CMD instead."
-    exit -1
-fi
-
 cd ~/Documents/Github/SLHPA-Web-App                     ; if [ $? -ne 0 ] ; then exit -6 ; fi
 time python -m venv venv                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
 time source venv/Scripts/activate                       ; if [ $? -ne 0 ] ; then exit -6 ; fi
@@ -14,8 +9,13 @@ time pip install -r mysite/requirements.txt             ; if [ $? -ne 0 ] ; then
 
 time ./run-pipeline.sh                                  ; if [ $? -ne 0 ] ; then exit -6 ; fi
 cd mysite                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
-python manage.py migrate                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
-python manage.py createsuperuser                        ; if [ $? -ne 0 ] ; then exit -6 ; fi
+time python manage.py migrate                           ; if [ $? -ne 0 ] ; then exit -6 ; fi
+if [ ! "$WINDIR" = "" ] ; then
+    echo "In CMD, run:"
+    echo "python manage.py createsuperuser"
+else
+    python manage.py createsuperuser                    ; if [ $? -ne 0 ] ; then exit -6 ; fi
+fi
 
 echo "Example command line to copy image files. They must be correctly distributed into numbered subdirectories."
 echo "time cp -r ~/Documents/'San Leandro Historical Photo Archive - 311MB/photos - 640x only'/*  ~/Documents/Github/SLHPA-Web-App/mysite/slhpa/static/slhpa/images/photos/"
