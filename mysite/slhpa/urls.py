8e53503775f813a005e2e2de7d9c139ef956fc26
CK <chris.keith@gmail.com>
2019-05-31 07:36:21 -0700

    Get around Django inability to migrate database if views have db queries.

--- mysite/slhpa/urls.py
+++ mysite/slhpa/urls.py
@@ -1,21 +1,27 @@
 from django.urls import path
 from django.conf import settings
+from django.db.utils import OperationalError
 
-from . import views
+try:
+    from . import views
 
-urlpatterns = [
-    path('', views.List.as_view(), name="index"),
-    path('detail/<str:pk>/', views.DetailView.as_view(), name='detail'),
-    path('data/<str:filename>', views.datafile, name='data_file'),
-    path('hello', views.hello, name='hello'),
-    path('edit/<str:id>/', views.edit, name='edit'),
-    path('delete/<str:id>/', views.delete, name='delete'),
-    path('add/', views.add, name='add'),
+    urlpatterns = [
+        path('', views.List.as_view(), name="index"),
+        path('detail/<str:pk>/', views.DetailView.as_view(), name='detail'),
+        path('data/<str:filename>', views.datafile, name='data_file'),
+        path('edit/<str:id>/', views.edit, name='edit'),
+        path('delete/<str:id>/', views.delete, name='delete'),
+        path('add/', views.add, name='add'),
 
-    # 'import' is a reserved word, can't do views.import .
-    path('import/<str:import_filename>', views.loaddb, name='loaddb'),
-    path('export/<str:export_filename>', views.export, name='export'),
-    path('compare/<str:resource_name>/', views.photo_compare, name='compare'),
-]
-
-
+        # 'import' is a reserved word, can't do views.import .
+        path('import/<str:import_filename>', views.loaddb, name='loaddb'),
+        path('export/<str:export_filename>', views.export, name='export'),
+        path('compare/<str:resource_name>/', views.photo_compare, name='compare'),
+    ]
+except OperationalError:
+    # https://stackoverflow.com/questions/34548768/django-no-such-table-exception
+    # happens when db doesn't exist yet, views.py should be importable without this side effect
+    from . import dummyview
+    urlpatterns = [
+        path('hello', dummyview.hello, name='hello'),
+    ]
