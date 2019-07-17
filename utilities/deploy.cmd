fa72a4baab5e2d7555af09e806e585c891c91d4f
CK <chris.keith@gmail.com>
2019-07-17 13:30:55 -0700

    Clarify some comments.

--- utilities/deploy.cmd
+++ utilities/deploy.cmd
@@ -1,9 +1,9 @@
 rem : CMD script because the gcloud command line utility (sometimes) doesn't echo in git bash.
 rem : python and gloud commands require user input, so this script cannot be run unattended. 
 rem : Takes some minutes or hours depending on the number of static files to be collected and uploaded.
-rem : It's recommended to first do "gcloud components update". This may take some minutes.
+rem : If I get a recommendation to "gcloud components update", I do it. This will take some minutes.
 
-pause "Set ALLOW_EDIT = False in mysite/mysite/settings.py unless import is needed."
+pause "Set 'GCP' variables at the end of mysite/mysite/settings.py (unless edit functionality is needed)."
 
 pushd %HOMEDRIVE%%HOMEPATH%\Documents\Github\SLHPA-Web-App\mysite\
                                                         IF %ERRORLEVEL% NEQ 0 goto :finish
@@ -11,6 +11,7 @@ time < ..\utilities\ret.txt
                                                         IF %ERRORLEVEL% NEQ 0 goto :finish
 cmd /c "gcloud config list"
                                                         IF %ERRORLEVEL% NEQ 0 goto :finish
+rem /c "gcloud config set project slhpa-03"
 cmd /c "python manage.py collectstatic"
                                                         IF %ERRORLEVEL% NEQ 0 goto :finish
 move /Y slhpa\static\slhpa\images\photos %TMP%
