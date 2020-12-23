b003d3981d380db47e3ced4aa8758557a2bc6085
Christopher Keith <chris.keith@gmail.com>
2020-12-23 09:48:19 -0800

    Add real data.

--- utilities/regex_tests.py
+++ utilities/regex_tests.py
@@ -8,7 +8,7 @@ patterns = [ r'\D(\d\d\d\d)\D'
             ]
 
 test_strings = ["1872", " 1872", " 1872 ", ".1872", 
-                ".1872.", "x1872", "x1872x", "ca1872", "ca.1872"]
+                ".1872.", "x1872", "x1872x", "ca1872", "ca.1872","E. 14th and Ward Sts., 1908"]
 
 for pattern in patterns:
     if re.findall(pattern, "18722"):
