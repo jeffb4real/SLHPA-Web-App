097dfec0c1c1b00af1c86a75629808fbe88abb4b
Christopher Keith <chris.keith@gmail.com>
2020-12-29 16:11:17 -0800

    Remove hardcoded path.

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,5 +1,3 @@
-cd ~/Documents/Github/SLHPA-web-app
-# python comber.py            ; if [ $? -ne 0 ] ; then exit -6 ; fi
 python merger.py            ; if [ $? -ne 0 ] ; then exit -6 ; fi
 python transformer.py       ; if [ $? -ne 0 ] ; then exit -6 ; fi
 python mapper.py            ; if [ $? -ne 0 ] ; then exit -6 ; fi
